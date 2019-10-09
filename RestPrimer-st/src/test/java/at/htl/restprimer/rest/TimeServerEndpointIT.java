a0d7142ea353f720acc2149bf0583b0556f12eab
schirinturko <schirin.turko24@gmail.com>
2019-10-09 20:07:34 +0200

    TimeServerEndpointIT Befehle hinzugefügt

--- RestPrimer-st/src/test/java/at/htl/restprimer/rest/TimeServerEndpointIT.java
+++ RestPrimer-st/src/test/java/at/htl/restprimer/rest/TimeServerEndpointIT.java
@@ -1,4 +1,32 @@
 package at.htl.restprimer.rest;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class TimeServerEndpointIT {
+
+    private Client client;
+    private WebTarget tut;
+
+    @Before
+    public void initClient(){
+        this.client = ClientBuilder.newClient();
+        this.tut = this.client.target("http://localhost:8080/restprimer/api/time");
+    }
+
+    @Test
+    public void fetchTime(){
+        Response response = this.tut.request(MediaType.TEXT_PLAIN).get();
+        assertThat(response.getStatus(), is(200));
+        String payload = response.readEntity(String.class);
+        assertThat(payload.startsWith("Time: "));
+    }
 }
